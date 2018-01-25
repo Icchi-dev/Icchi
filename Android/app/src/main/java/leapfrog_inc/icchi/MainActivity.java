01bc6ca460d263db3fd084459dca0f387c3ba07f
SchoolChatDev <schoolchatdev@yahoo.co.jp>
2018-01-25 20:59:56 +0900

    [Android] Facebookログインを実装

--- Android/app/src/main/java/leapfrog_inc/icchi/MainActivity.java
+++ Android/app/src/main/java/leapfrog_inc/icchi/MainActivity.java
@@ -1,13 +1,53 @@
 package leapfrog_inc.icchi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("", "");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("", "");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("", "");
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
