e1eac3e9631d70de9c563421c5b9e2da74671847
Leapfrog-Software <Leapfrog1357>
2018-03-17 15:25:21 +0900

    プロフィール画面の好き・嫌い一覧を横スワイプ式に変更

--- Android/app/src/main/java/leapfrog_inc/icchi/Function/CommonUtility.java
+++ Android/app/src/main/java/leapfrog_inc/icchi/Function/CommonUtility.java
@@ -1,14 +1,29 @@
 package leapfrog_inc.icchi.Function;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.util.Size;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
+
+import static android.content.Context.WINDOW_SERVICE;
 
 /**
  * Created by Leapfrog-Software on 2018/01/26.
  */
 
 public class CommonUtility {
+
+    public static Point getWindowSize(Activity activity) {
+
+        WindowManager wm = (WindowManager)activity.getSystemService(WINDOW_SERVICE);
+        Display disp = wm.getDefaultDisplay();
+
+        Point size = new Point();
+        disp.getSize(size);
+        return size;
+    }
 
     public static float getDeviceDensity(Activity activity){
         return activity.getResources().getDisplayMetrics().density;
